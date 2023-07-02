5e0aa7576b943c6872f66708a930ea9172915a95
Еприцкий_Денис_8И22 <dae29@tpu.ru>
2023-07-02 15:00:30 +0800

    убрал ошибки PEP8

--- branch_1/1_3.py
+++ branch_1/1_3.py
@@ -1,10 +1,10 @@
-d1=int(input("Число 1 - "))
-d2=int(input("Число 2 - "))
-d3=int(input("Число 3 - "))
-ma=max(d1,d2,d3)
-mi=min(d1,d2,d3)
-su=d1+d2+d3
-midl=su-ma-mi
+d1 = int(input("Число 1 - "))
+d2 = int(input("Число 2 - "))
+d3 = int(input("Число 3 - "))
+ma = max(d1, d2, d3)
+mi = min(d1, d2, d3)
+su = d1 + d2 + d3
+midl = su - ma - mi
 print(f"Число {ma} является наибольшим")
 print(ma)
 print(midl)
--- branch_1/1_8.py
+++ branch_1/1_8.py
@@ -1,4 +1,4 @@
-name=str(input("Введите имя "))
-age=int(input("Введите возраст "))
+name = str(input("Введите имя "))
+age = int(input("Введите возраст "))
 
 print(f"Привет {name}! Тебе уже {str(age)} лет.")
